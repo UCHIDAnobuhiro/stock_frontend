968151853de7acb3bbe2395d3fa79ece7500aa9c
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-10-11 22:35:25 +0900

    chore:構成を変更

--- app/src/main/java/com/example/stock/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stock/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.stock.theme
+package com.example.stock.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun TodoAppTheme(
+fun StockTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
--- app/src/main/java/com/example/stock/ui/theme/Type.kt
+++ app/src/main/java/com/example/stock/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.stock.theme
+package com.example.stock.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
